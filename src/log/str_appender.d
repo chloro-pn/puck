5bb190ce02f6e3b57727f3f0d613320a5b0dc903
chloro-pn <13125187405@163.com>
2020-06-01 16:52:57 +0800

    c

--- src/log/str_appender.d
+++
@@ -1,6 +0,0 @@
-str_appender.o: str_appender.cpp ../../include/log/str_appender.h \
- ../../include/log/convert.h
-
-../../include/log/str_appender.h:
-
-../../include/log/convert.h:
--- src/third_party/md5.d
+++
@@ -1,4 +0,0 @@
-third_party/md5.o: third_party/md5.cpp \
- third_party/../../third_party/md5.h
-
-third_party/../../third_party/md5.h:
